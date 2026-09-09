cc922954a4b1fbf454803b2ee21b303404ca7f75
Camilo Florez <camiloflorez@Camilos-MacBook-Air.local>
2026-09-09 09:32:10 -0400

    Fix Mac updater USB log predicate

--- firmware/capture_updater_usb_log.sh
+++ firmware/capture_updater_usb_log.sh
@@ -5,6 +5,6 @@ out=${1:-/tmp/mx490-updater-usb.log}
 echo "Recording macOS USB and Canon updater logs to $out"
 echo "Launch the Canon updater and stop after it displays the current version."
 echo "Press Ctrl-C here before clicking Start."
-exec log stream --style compact --level debug \
-  --predicate '(subsystem CONTAINS[c] "USB" OR process CONTAINS[c] "Printer Update" OR process CONTAINS[c] "CIJ")' \
+exec /usr/bin/log stream --style compact --level debug \
+  --predicate 'subsystem == "com.apple.iokit.IOUSBHostFamily" OR process CONTAINS[c] "Printer Update" OR process CONTAINS[c] "CIJ"' \
   > "$out"
